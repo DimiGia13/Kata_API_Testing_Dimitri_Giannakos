d3a62186763d756e616e8c4613dfa6e88713a579
DimiGia13 <dimitri.giannakos13@icloud.com>
2026-01-29 11:04:17 +0100

    implement first booking API scenario (GET all bookings)

---
+++ src/test/java/com/booking/stepdefinitions/BookingSteps.java
@@ -0,0 +1,37 @@
+package com.booking.stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.greaterThan;
+
+public class BookingSteps {
+
+    private Response response;
+
+    @Given("the Booking API is available")
+    public void theBookingAPIIsAvailable(){
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    }
+
+    //region GET booking scenario
+    @When("^I send a GET request to /booking$")
+    public void iSendAGetRequestToBooking() {
+        response = when().get("/booking");
+    }
+
+    @Then("the response status code should be 200")
+    public void theResponseStatusCodeShouldBe200(){
+        response.then().statusCode(200);
+    }
+
+    @And("the response should contain at least one booking")
+    public void theResponseShouldContainAtLeastOneBooking() {
+        response.then().body("size()", greaterThan(0));
+    }
+    //endregion
+}
---
+++ src/test/resources/features/booking.feature
@@ -0,0 +1,8 @@
+Feature: Booking Api
+
+  @booking @smoke
+  Scenario: View all the bookings
+    Given the Booking API is available
+    When I send a GET request to /booking
+    Then the response status code should be 200
+    And the response should contain at least one booking
